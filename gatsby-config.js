2f27db98e33469f7cc0bc2f316ebcb806ec5f342
Joel Morales (naojamg) <nao5.2130@gmail.com>
2021-02-04 01:23:53 -0600

    Se agregan configuraciones para strapi y demo de consumo de datos

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        contentTypes: [
+          "restaurant",
+        ],
+        queryLimit: 1000,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,32 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { StaticQuery, graphql } from 'gatsby';
+
+const query = graphql`
+  query {
+    allStrapiRestaurant {
+      edges {
+        node {
+          strapiId
+          name
+          description
+        }
+      }
+    }
+  }
+`;
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  <StaticQuery
+    query={query}
+    render={data => (
+      <ul>
+        {data.allStrapiRestaurant.edges.map(restaurant => (
+          <li key={restaurant.node.strapiId}>{restaurant.node.name}</li>
+        ))}
+      </ul>
+    )}
+  />
+);
 
-export default IndexPage
+export default IndexPage;
