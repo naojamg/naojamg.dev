ad4e4dd42f10392bae5612bb4283829c1f4686a4
Joel Morales (naojamg) <nao5.2130@gmail.com>
2021-02-09 18:36:13 -0600

    Cambio en estilos e imagenes de redes sociales

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,16 +14,16 @@ const Footer = ({ text, backgroundColor, color }) => {
       <p>{`Joel Morales @naojamg | Todos los derechos reservados © ${year}.`}</p>
       <div className="socialContainer">
         <Link to={data.allStrapiUser.edges[0].node.platzi} target="_blank" title="Platzi" rel="noopener">
-          <img className="socialLink" src={Platzi} alt="Platzi" title="Platzi" />
+          <img className="socialLink" width="50" height="50" src={Platzi} alt="Platzi" title="Platzi" />
         </Link>
         <Link to={data.allStrapiUser.edges[0].node.twitter} target="_blank" title="Twitter" rel="noopener">
-          <img className="socialLink" src={Twitter} alt="Twitter" title="Twitter" />
+          <img className="socialLink" width="50" height="50" src={Twitter} alt="Twitter" title="Twitter" />
         </Link>
         <Link to={data.allStrapiUser.edges[0].node.github} target="_blank" title="Github" rel="noopener">
-          <img className="socialLink" src={Github} alt="Github" title="Github" />
+          <img className="socialLink" width="50" height="50" src={Github} alt="Github" title="Github" />
         </Link>
         <Link to={data.allStrapiUser.edges[0].node.buyCoffe} target="_blank" title="Buy me a Coffe" rel="noopener">
-          <img className="socialLink" src={BuyCoffe} alt="Buy me a Coffe" title="Buy me a Coffe" />
+          <img className="socialLink" width="50" height="50" src={BuyCoffe} alt="Buy me a Coffe" title="Buy me a Coffe" />
         </Link>
       </div>
     </footer>
