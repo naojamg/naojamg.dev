19ffae631827414b2a8101225fa1a6941bfb2236
Joel Morales (naojamg) <nao5.2130@gmail.com>
2021-02-11 17:12:45 -0600

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-6c3ead24af170cdcb87c.js"
   },
   {
-    "url": "app-f29cd78148b571681710.js"
+    "url": "app-923bcc7d2583de7fffc2.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7d5c0803e108a7ba87be65bc42cf61bb"
+    "revision": "11807eb43c15eab05e6cb8ff3db3e689"
   },
   {
     "url": "static/webfonts/s/lato/v17/S6uyw4BMUTPHjx4wXg.woff2"
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-f29cd78148b571681710.js`))) {
+  if (!resources || !(await caches.match(`/app-923bcc7d2583de7fffc2.js`))) {
     return await fetch(event.request)
   }
 
