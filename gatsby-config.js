fba1a7043abfaabc9c4f773cda8f36f03745c62e
Joel Morales (naojamg) <nao5.2130@gmail.com>
2021-02-11 17:16:28 -0600

    Se agrega id para google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,5 +90,22 @@ module.exports = {
         defer: false,
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "G-7MQQ1CZ8SC",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
   ],
 }
