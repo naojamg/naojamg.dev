1fa2875979be90b077e92041b9ada745fe251735
Joel Morales (naojamg) <nao5.2130@gmail.com>
2021-02-11 17:13:04 -0600

    Cambio en id de google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,20 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "G-WRN6Q7QMD2",
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-7MQQ1CZ8SC",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
       },
     },
   ],
