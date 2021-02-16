75e369bfec86bd426bc9033f4ea0f76bf0ae9418
Joel Morales (naojamg) <nao5.2130@gmail.com>
2021-02-16 04:25:08 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d30d2ee0b020202dbafa.js"
+    "url": "webpack-runtime-3c2dd1474a94dda7b8e0.js"
   },
   {
     "url": "framework-6c3ead24af170cdcb87c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e11479169a0389f53d2055634c4ddb3a"
+    "revision": "f56203a48ab56db5b8b1d0bdccbf246a"
   },
   {
     "url": "static/webfonts/s/lato/v17/S6uyw4BMUTPHjx4wXg.woff2"
